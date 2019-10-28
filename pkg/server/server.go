7f9841a32fddc71dec108f5812f6bdf1f40c6ff5
zengfanhua <zengfanhua@brilliance.com.cn>
2019-10-28 16:11:02 +0800

    server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"sync"
+
 	"github.com/tinywell/chatroom/pkg/proto"
 )
 
@@ -12,18 +14,51 @@ type Client struct {
 
 // Server is the chatroom server
 type Server struct {
+	clients map[string]*Client
+	mutex   sync.RWMutex
 }
 
 // NewServer return a new Server
 func NewServer() *Server {
-	return nil
-}
-
-func (s *Server) start() {
-
+	return &Server{
+		clients: make(map[string]*Client),
+	}
 }
 
 // Chat is implement for proto.ChatRoomServer
 func (s *Server) Chat(stream proto.ChatRoom_ChatServer) {
+	for {
+		msg := &proto.Message{}
+		err := stream.RecvMsg(msg)
+		if err != nil {
+			return
+		}
+		switch msg.Payload.(type) {
+		case *proto.Message_Chatmsg:
+			s.broadCast(msg)
+		case *proto.Message_Signin:
+			m := msg.Payload.(*proto.Message_Signin)
+			s.newClient(m.Signin.Name, stream)
+		default:
+			return
+		}
+	}
+}
 
+func (s *Server) newClient(name string, stream proto.ChatRoom_ChatServer) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	client := &Client{
+		name:   name,
+		stream: stream,
+	}
+	s.clients[name] = client
+}
+func (s *Server) broadCast(msg *proto.Message) {
+	for _, c := range s.clients {
+		err := c.stream.SendMsg(msg)
+		if err != nil {
+			continue
+		}
+	}
 }
